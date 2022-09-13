33f444bd61a1202491297d627f0b5e01b661cabe
BerthonyBeya <berthonybeyagithub@gmail.com>
2022-09-13 17:03:20 +0530

    Navbar completed

--- src/HomeCarousel/HomeCarousel.js
+++ src/HomeCarousel/HomeCarousel.js
@@ -1,25 +1,20 @@
 import "./HomeCarousel.scss";
 import { Carousel } from "react-bootstrap";
 
-
 const HomeCarousel = () => {
   return (
     <>
       <div className="carousel-overlay"></div>
-      <Carousel interval={2000}>
+      <div className="carousel__content">
+        <h1>This is the content</h1>
+      </div>
+      <Carousel interval={1000000} nextIcon={false} prevIcon={false}>
         <Carousel.Item>
           <img
             className="carousel-image"
             src="images/Avengers.jpg"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
-              eaque voluptatum, ut quasi fugiat nesciunt! Deserunt blanditiis
-              repellat autem voluptatum?
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
