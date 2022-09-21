073eea14195773260247c961ab92b5b89c19c3ad
BerthonyBeya <berthonybeyagithub@gmail.com>
2022-09-21 22:35:21 +0530

    Carousel improvement

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,17 +5,17 @@ const Navbar = () => {
   return (
     <div className="container-box">
       <nav className="navbar">
-        <ul className="navbar__ul">
-          <li className="navbar__li">
+        <ul className="navbar-ul">
+          <li className="navbar-ul__link">
             <a href="/">Home</a>
           </li>
-          <li className="navbar__li">
+          <li className="navbar-ul__link">
             <a href="/movie">Movies</a>
           </li>
-          <li className="navbar__li">
+          <li className="navbar-ul__link">
             <a href="/tvshows">Tv Shows</a>
           </li>
-          <li className="navbar__li">
+          <li className="navbar-ul__link">
             <a href="/about">About</a>
           </li>
         </ul>
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,14 +1,32 @@
 import "./SearchBar.scss";
 import { BsSearch } from "react-icons/bs";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [color, setColor] = useState("#fff");
+
+  const colorPrimaryHandler = () => {
+    setColor("rgb(0, 12, 24)");
+  };
+
+  const colorWhiteHandler = () => {
+    setColor("#fff");
+  };
+
   return (
-    <form className="search-bar">
+    <form
+      className="search-bar"
+      onMouseEnter={colorPrimaryHandler}
+      onMouseLeave={colorWhiteHandler}
+    >
       <button type="submit">
-        <BsSearch className="search-bar__icon" />
+        <BsSearch color={color} className="search-bar__icon" />
       </button>
 
-      <input type="text" placeholder="Enter your keywords..." />
+      <input
+        type="text"
+        placeholder="Enter your keywords..."
+      />
     </form>
   );
 };
