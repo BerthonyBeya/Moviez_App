c9097ad81469fb01dfa2af5f29c37bd544fc2bb6
BerthonyBeya <berthonybeyagithub@gmail.com>
2022-09-22 21:49:37 +0530

    Carousel buttons animation added

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,26 +1,13 @@
 import "./SearchBar.scss";
 import { BsSearch } from "react-icons/bs";
-import { useState } from "react";
 
 const SearchBar = () => {
-  const [color, setColor] = useState("#fff");
-
-  const colorPrimaryHandler = () => {
-    setColor("rgb(0, 12, 24)");
-  };
-
-  const colorWhiteHandler = () => {
-    setColor("#fff");
-  };
-
   return (
     <form
       className="search-bar"
-      onMouseEnter={colorPrimaryHandler}
-      onMouseLeave={colorWhiteHandler}
     >
       <button type="submit">
-        <BsSearch color={color} className="search-bar__icon" />
+        <BsSearch className="search-bar__icon" />  
       </button>
 
       <input
