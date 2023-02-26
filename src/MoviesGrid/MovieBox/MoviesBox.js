8cb9cd95326ed6997f15b6afe02449ba5da25c77
BerthonyBeya <berthonybeyagithub@gmail.com>
2023-02-26 18:23:47 +0530

    component clean up

---
+++ src/MoviesGrid/MovieBox/MoviesBox.js
@@ -0,0 +1,45 @@
+import "../MoviesGrid.scss";
+import { FaPlay, FaClipboardList } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+
+const MoviesBox = (props) => {
+  return (
+    <div className="movies-grid__movie">
+      <div className="movies-grid__quality">
+        <div>
+          {" "}
+          <p>HD</p>
+        </div>
+      </div>
+      <div className="movies-grid__overlay">
+        <div>
+          <button className="carousel-movie-buttons__styling carousel-movie-buttons--watch">
+            <p>
+              <FaPlay className="carousel-movie-buttons__watch__icon" />
+              Watch now
+            </p>
+          </button>
+        </div>
+        <div>
+          <button className="carousel-movie-buttons__styling carousel-movie-buttons--add">
+            <p>
+              <AiOutlineHeart className="carousel-movie-buttons__heart__icon" />
+              Add to list
+            </p>
+          </button>
+        </div>
+        <div>
+          <button className="carousel-movie-buttons__styling carousel-movie-buttons--add">
+            <p>
+              <FaClipboardList className="carousel-movie-buttons__heart__icon" />
+              Details
+            </p>
+          </button>
+        </div>
+      </div>
+      <img className="movies-grid__movie__img" src={props.poster} alt="img" />
+    </div>
+  );
+};
+
+export default MoviesBox;
--- src/MoviesGrid/MoviesGrid.js
+++ src/MoviesGrid/MoviesGrid.js
@@ -1,16 +1,11 @@
 import "./MoviesGrid.scss";
+import MoviesBox from "./MovieBox/MoviesBox";
 import { useSelector } from "react-redux";
-import { FaStar, FaPlay, FaClipboardList } from "react-icons/fa";
-import { AiOutlineHeart } from "react-icons/ai";
-
 
 const MoviesGrid = (props) => {
-  // Geeting movies from redux
   const movies = useSelector((state) => {
     return state.movies.value;
   });
-
-
   return (
     <div className="movies">
       <span>
@@ -22,47 +17,7 @@ const MoviesGrid = (props) => {
       <div className="movies-grid">
         {movies?.movies
           ? movies.movies.map((el) => {
-              return (
-                <div className="movies-grid__movie">
-                  <div className="movies-grid__quality">
-                    <div>
-                      {" "}
-                      <p>HD</p>
-                    </div>
-                  </div>
-                  <div className="movies-grid__overlay">
-                    <div>
-                      <button className="carousel-movie-buttons__styling carousel-movie-buttons--watch">
-                        <p>
-                          <FaPlay className="carousel-movie-buttons__watch__icon" />
-                          Watch now
-                        </p>
-                      </button>
-                    </div>
-                    <div>
-                      <button className="carousel-movie-buttons__styling carousel-movie-buttons--add">
-                        <p>
-                          <AiOutlineHeart className="carousel-movie-buttons__heart__icon" />
-                          Add to list
-                        </p>
-                      </button>
-                    </div>
-                    <div>
-                      <button className="carousel-movie-buttons__styling carousel-movie-buttons--add">
-                        <p>
-                          <FaClipboardList className="carousel-movie-buttons__heart__icon" />
-                          Details
-                        </p>
-                      </button>
-                    </div>
-                  </div>
-                  <img
-                    className="movies-grid__movie__img"
-                    src={el.Poster}
-                    alt="img"
-                  />
-                </div>
-              );
+              return <MoviesBox poster={el.Poster} />;
             })
           : ""}
       </div>
