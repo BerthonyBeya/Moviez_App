c5a1a743373856be0be5f93d0dce0c4dc7ed5e5b
BerthonyBeya <berthonybeyagithub@gmail.com>
2023-09-23 04:52:54 +0530

    Carousel improved

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,17 +1,9 @@
 import "./SearchBar.scss";
 import { BsSearch } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
-  /* const navigate = useNavigate();
-
-  const searchHandler = (event) => {
-    event.preventDefault();
-    navigate("/nowplayingmovies");
-  }; */
-
   return (
-    <form onSubmit={() => {}} className="search-bar">
+    <form className="search-bar">
       <button type="submit">
         <BsSearch className="search-bar__icon" />
       </button>
