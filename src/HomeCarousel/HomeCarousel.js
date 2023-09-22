ba0acb78c76087e9f07fd122d41e5fc6b3332545
BerthonyBeya <berthonybeyagithub@gmail.com>
2023-09-23 04:43:08 +0530

    Carousel improved

--- src/HomeCarousel/HomeCarousel.js
+++ src/HomeCarousel/HomeCarousel.js
@@ -9,13 +9,7 @@ import "./HomeCarousel.scss";
 const HomeCarousel = (props) => {
   const [homeMovies, setHomeMovies] = useState([]);
   const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
-
-  const nowPlayingTvShows = [
-    "images/Nowplayingtvshows1-min.jpg",
-    "images/Nowplayingtvshows2-min.jpg",
-    "images/Nowplayingtvshows3-min.jpg",
-    "images/Nowplayingtvshows4-min.jpg",
-  ];
+  const [nowPlayingTvShows, setNowPlayingTvShows] = useState([]);
 
   // Fetching Carousel data
   useEffect(() => {
@@ -28,7 +22,7 @@ const HomeCarousel = (props) => {
       ];
 
       // Get home movies
-      homeMovieTitles.map((movie) => {
+      homeMovieTitles.forEach((movie) => {
         const fetchMovie = async () => {
           const result = await axios(
             `https://api.themoviedb.org/3/search/movie?api_key=770df377767ac6b055c68672f960c59f&query=${movie}&include_adult=false&language=en-US&page=1`
@@ -52,7 +46,7 @@ const HomeCarousel = (props) => {
         "carl's date",
       ];
 
-      nowPlayingMoviesTitles.map((movie) => {
+      nowPlayingMoviesTitles.forEach((movie) => {
         const fetchMovie = async () => {
           const result = await axios(
             `https://api.themoviedb.org/3/search/movie?api_key=770df377767ac6b055c68672f960c59f&query=${movie}&include_adult=false&language=en-US&page=1`
@@ -67,11 +61,36 @@ const HomeCarousel = (props) => {
       });
     };
 
+    // Get now playing tv shows
+    const searchNowPlayingTvShows = async () => {
+      const nowPlayingTvShowsTitles = [
+        "The challenge usa",
+        "The winter king",
+        "The wheel of time",
+        "the chi",
+      ];
+
+      nowPlayingTvShowsTitles.forEach((show) => {
+        const fetchShow = async () => {
+          const result = await axios(
+            `https://api.themoviedb.org/3/search/tv?api_key=770df377767ac6b055c68672f960c59f&query=${show}&include_adult=false&language=en-US&page=1`
+          );
+
+          setNowPlayingTvShows((currentState) => {
+            return [...currentState, result.data.results[0]];
+          });
+        };
+
+        fetchShow();
+      });
+    };
+
     searchHomeMovies();
     searchNowPlayingMovies();
+    searchNowPlayingTvShows();
   }, []);
 
-  console.log(homeMovies);
+  
 
   const addingCarouselImages = () => {
     // Display this Carousel Infos if it's "HomePage"
@@ -147,27 +166,25 @@ const HomeCarousel = (props) => {
               alt="First slide"
             />
             <Carousel.Caption className="carousel-movie">
-              <h1 className="carousel-movie__title">Avengers: Endgame</h1>
+              <h1 className="carousel-movie__title">{movie.original_title}</h1>
               <div className="carousel-movie-details">
-                <div className="carousel-movie-details__quality">
+                {/* <div className="carousel-movie-details__quality">
                   <div>
                     {" "}
                     <p>HD</p>
                   </div>
-                </div>
+                </div> */}
                 <p className="carousel-movie-details__rate">
-                  <FaStar /> 8.30
+                  <FaStar /> {movie.vote_average}
                 </p>
-                <p className="carousel-movie-details__minutes">148 min</p>
-                <p className="carousel-movie__type">Action</p>
+                <p className="carousel-movie-details__release">
+                  {movie.release_date}
+                </p>
+                <p className="/* carousel-movie__language */">
+                  language: {movie.original_language}
+                </p>
               </div>
-              <p className="carousel-movie__bio">
-                After half of all life is snapped away by Thanos, the Avengers
-                are left scattered and divided. Now with a way to reverse the
-                damage, the Avengers and their allies must assemble once more
-                and learn to put differences aside in order to work
-                together.....
-              </p>
+              <p className="carousel-movie__bio">{movie.overview}</p>
               <div className="carousel-movie-buttons">
                 <span>
                   <button className="carousel-movie-buttons__styling carousel-movie-buttons--watch">
@@ -202,36 +219,34 @@ const HomeCarousel = (props) => {
 
     // Display this carousel if it's "Now Playing Shows" Page
     if (props.type === "Now Playing Shows") {
-      return nowPlayingTvShows.map((movie) => {
+      return nowPlayingTvShows.map((show) => {
         return (
           <Carousel.Item key={uuid()}>
             <img
               className="carousel__image"
-              src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}
+              src={`https://image.tmdb.org/t/p/w780/${show.poster_path}`}
               alt="First slide"
             />
             <Carousel.Caption className="carousel-movie">
-              <h1 className="carousel-movie__title">Avengers: Endgame</h1>
+              <h1 className="carousel-movie__title">{show.original_name}</h1>
               <div className="carousel-movie-details">
-                <div className="carousel-movie-details__quality">
+                {/* <div className="carousel-movie-details__quality">
                   <div>
                     {" "}
                     <p>HD</p>
                   </div>
-                </div>
+                </div> */}
                 <p className="carousel-movie-details__rate">
-                  <FaStar /> 8.30
+                  <FaStar /> {show.vote_average}
                 </p>
-                <p className="carousel-movie-details__minutes">148 min</p>
-                <p className="carousel-movie__type">Action</p>
+                <p className="carousel-movie-details__release">
+                  {show.first_air_date}
+                </p>
+                <p className="/* carousel-movie__language */">
+                  language: {show.original_language}
+                </p>
               </div>
-              <p className="carousel-movie__bio">
-                After half of all life is snapped away by Thanos, the Avengers
-                are left scattered and divided. Now with a way to reverse the
-                damage, the Avengers and their allies must assemble once more
-                and learn to put differences aside in order to work
-                together.....
-              </p>
+              <p className="carousel-movie__bio">{show.overview}</p>
               <div className="carousel-movie-buttons">
                 <span>
                   <button className="carousel-movie-buttons__styling carousel-movie-buttons--watch">
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,19 +1,22 @@
 import "./SearchBar.scss";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  /* const navigate = useNavigate();
+
+  const searchHandler = (event) => {
+    event.preventDefault();
+    navigate("/nowplayingmovies");
+  }; */
+
   return (
-    <form
-      className="search-bar"
-    >
+    <form onSubmit={() => {}} className="search-bar">
       <button type="submit">
-        <BsSearch className="search-bar__icon" />  
+        <BsSearch className="search-bar__icon" />
       </button>
 
-      <input
-        type="text"
-        placeholder="Enter your keywords..."
-      />
+      <input type="text" placeholder="Enter your keywords..." />
     </form>
   );
 };
